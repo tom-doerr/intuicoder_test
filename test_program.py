8bb46eecc2cc1f700ecbd8cd9d0c551e720e81bc
Tom Dörr <tomdoerr96@gmail.com>
2024-05-11 12:54:38 +0200

    Add bitcoin price check to test_program.py.

--- test_program.py
+++ test_program.py
@@ -6,7 +6,14 @@ os.system('ls -l')
 # check system time
 os.system('date')
 
-#- get list of files in dir
+##- get list of files in dir
+file_list = os.listdir()
+print(file_list)
 assert 'LICENSE' in file_list
 
 print(file_list)
+
+#- get the bitcoin price
+assert bitcoin_value > 60000
+
+print("bitcoin_value:", bitcoin_value)
